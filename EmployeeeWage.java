97bd9efa09d59e8f0d91dc1abf9b34718fe79dc8
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-26 12:01:53 +0530

    Assume 20 Working day per month

--- EmployeeeWage.java
+++ EmployeeeWage.java
@@ -1,24 +1,28 @@
 public class EmployeeeWage {
-    public static int Part_time = 8,Wage_per_Hour = 20,Full_day_Hour = 8,Total_wage=0;
+    public static int Days = 0,Part_time = 8,Wage_per_Hour = 20,Full_day_Hour = 8,Total_wage=0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
-        int status = (int)(Math.random()*3);
-        switch (status ) {
-            case 0: {
-                System.out.println("employee is Absent");
-                break;
+        while (Days < 20) {
+            int status = (int) (Math.random() * 3);
+
+            switch (status) {
+                case 0: {
+                    System.out.println("employee is Absent");
+                    break;
+                }
+                case 1: {
+                    System.out.println("employee is present");
+                    Total_wage += Full_day_Hour * Wage_per_Hour;
+                    break;
+                }
+                default: {
+                    System.out.println("Part time");
+                    Total_wage += Wage_per_Hour * Part_time;
+                    break;
+                }
             }
-            case 1: {
-                System.out.println("employee is present");
-                Total_wage = Full_day_Hour * Wage_per_Hour;
-                break;
-            }
-            default: {
-                System.out.println("Part time");
-                Total_wage = Wage_per_Hour * Part_time;
-                break;
-            }
+            Days++;
+            System.out.println(Total_wage);
         }
-        System.out.println(Total_wage);
     }
 }
