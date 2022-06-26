f2a478a89bf448f2eabb250ceb561946ccf6c82f
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-26 20:33:28 +0530

    Assume 100 hours and 20 Days

--- EmployeeeWage.java
+++ EmployeeeWage.java
@@ -1,9 +1,15 @@
 public class EmployeeeWage {
-    public static int Days = 0,Part_time = 8,Wage_per_Hour = 20,Full_day_Hour = 8,Total_wage=0;
+    public static int Total_Hours=100,Days = 20,Part_time = 8,Wage_per_Hour = 20,Full_day_Hour = 8,Total_wage=0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
-        while (Days < 20) {
+        while (Days > 0 && Total_Hours > 0) {
             int status = (int) (Math.random() * 3);
+            if(Total_Hours < 8 && status != 0){
+                System.out.println("remaining Hours  "+ Days);
+                Total_wage += Wage_per_Hour * Total_Hours;
+                System.out.println(Total_wage);
+                break;
+            }
 
             switch (status) {
                 case 0: {
@@ -13,15 +19,18 @@ public class EmployeeeWage {
                 case 1: {
                     System.out.println("employee is present");
                     Total_wage += Full_day_Hour * Wage_per_Hour;
+                    Total_Hours -= Full_day_Hour;
                     break;
                 }
                 default: {
                     System.out.println("Part time");
                     Total_wage += Wage_per_Hour * Part_time;
+                    Total_Hours -= Full_day_Hour;
                     break;
                 }
             }
-            Days++;
+            Days--;
+            System.out.println("remaining Hours  "+ Days);
             System.out.println(Total_wage);
         }
     }
