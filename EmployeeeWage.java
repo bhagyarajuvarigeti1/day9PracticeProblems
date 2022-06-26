6f0f905400488489e96cf0c69a34703b9fd39783
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-26 11:54:48 +0530

    UC4 branch

--- EmployeeeWage.java
+++ EmployeeeWage.java
@@ -3,16 +3,21 @@ public class EmployeeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
         int status = (int)(Math.random()*3);
-        if(status == 0){
-            System.out.println("employee is Absent");
-        }
-        else if(status == 1){
-            System.out.println("employee is present");
-            Total_wage = Full_day_Hour*Wage_per_Hour;
-        }
-        else{
-            System.out.println("Part time");
-            Total_wage = Wage_per_Hour*Part_time;
+        switch (status ) {
+            case 0: {
+                System.out.println("employee is Absent");
+                break;
+            }
+            case 1: {
+                System.out.println("employee is present");
+                Total_wage = Full_day_Hour * Wage_per_Hour;
+                break;
+            }
+            default: {
+                System.out.println("Part time");
+                Total_wage = Wage_per_Hour * Part_time;
+                break;
+            }
         }
         System.out.println(Total_wage);
     }
