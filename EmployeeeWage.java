c222fbb9439d69957df5d29fd6cd4b22c16475e7
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-26 20:42:38 +0530

    use class method and class variables

--- EmployeeeWage.java
+++ EmployeeeWage.java
@@ -2,6 +2,9 @@ public class EmployeeeWage {
     public static int Total_Hours=100,Days = 20,Part_time = 8,Wage_per_Hour = 20,Full_day_Hour = 8,Total_wage=0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
+        employeeWage(Wage_per_Hour,Full_day_Hour,Part_time);
+    }
+    public static void employeeWage(int Wage_per_hour,int Fulldayhour,int parttime){
         while (Days > 0 && Total_Hours > 0) {
             int status = (int) (Math.random() * 3);
             if(Total_Hours < 8 && status != 0){
